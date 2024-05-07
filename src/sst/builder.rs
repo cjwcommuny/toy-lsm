c567f0b3296ef52c9dc765469650ca73bf13dd66
cjw <cjwcommuny@outlook.com>
2024-05-07 13:47:30 +0800

    Revert "[Weekly run] Add Notice to New Files" (#23)
    This reverts commit dcafb26aa59f52496fee6a0e1b4809f5c930122b.

--- src/sst/builder.rs
+++ src/sst/builder.rs
@@ -1,6 +1,3 @@
-// SPDX-License-Identifier: CC0-1.0 OR MIT OR Apache-2.0
-// -> todo! remove this notice
-
 use std::mem;
 use std::ops::Bound::Unbounded;
 use std::sync::Arc;
--- src/sst/iterator/mod.rs
+++ src/sst/iterator/mod.rs
@@ -1,6 +1,3 @@
-// SPDX-License-Identifier: CC0-1.0 OR MIT OR Apache-2.0
-// -> todo! remove this notice
-
 pub mod concat;
 pub mod iter;
 pub mod merged;
