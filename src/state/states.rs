6f7ff34168064ea844c8a6a9c0abcd2736435dc6
Kermit <chenjiawei1@xiaohongshu.com>
2024-08-14 22:52:22 +0800

    fix: test

--- src/state/states.rs
+++ src/state/states.rs
@@ -972,8 +972,8 @@ mod test {
         3     -
         ----------- 3
         -    4
+         */
 
-                */
         let snapshot0 = storage.new_txn().unwrap();
         storage.put_for_test(b"a", b"1").await.unwrap();
         storage.put_for_test(b"b", b"1").await.unwrap();
@@ -992,6 +992,7 @@ mod test {
 
         {
             let guard = storage.state_lock.lock().await;
+            storage.force_freeze_memtable(&guard).await.unwrap();
             storage.force_flush_imm_memtable(&guard).await.unwrap();
             storage.force_compact(&guard).await.unwrap();
         }
@@ -1378,4 +1379,66 @@ mod test {
 
         LsmStorageState::new(options, persistent).await.unwrap()
     }
+
+    // todo: week 3, day 7 test
+    // #[test]
+    // fn test_task3_mvcc_compaction() {
+    //     let dir = tempdir().unwrap();
+    //     let options = LsmStorageOptions::default_for_week2_test(CompactionOptions::NoCompaction);
+    //     let storage = MiniLsm::open(&dir, options.clone()).unwrap();
+    //     storage
+    //         .write_batch(&[
+    //             WriteBatchRecord::Put("table1_a", "1"),
+    //             WriteBatchRecord::Put("table1_b", "1"),
+    //             WriteBatchRecord::Put("table1_c", "1"),
+    //             WriteBatchRecord::Put("table2_a", "1"),
+    //             WriteBatchRecord::Put("table2_b", "1"),
+    //             WriteBatchRecord::Put("table2_c", "1"),
+    //         ])
+    //         .unwrap();
+    //     storage.force_flush().unwrap();
+    //     let snapshot0 = storage.new_txn().unwrap();
+    //     storage
+    //         .write_batch(&[
+    //             WriteBatchRecord::Put("table1_a", "2"),
+    //             WriteBatchRecord::Del("table1_b"),
+    //             WriteBatchRecord::Put("table1_c", "2"),
+    //             WriteBatchRecord::Put("table2_a", "2"),
+    //             WriteBatchRecord::Del("table2_b"),
+    //             WriteBatchRecord::Put("table2_c", "2"),
+    //         ])
+    //         .unwrap();
+    //     storage.force_flush().unwrap();
+    //     storage.add_compaction_filter(CompactionFilter::Prefix(Bytes::from("table2_")));
+    //     storage.force_full_compaction().unwrap();
+    //
+    //     let mut iter = construct_merge_iterator_over_storage(&storage.inner.state.read());
+    //     check_iter_result_by_key(
+    //         &mut iter,
+    //         vec![
+    //             (Bytes::from("table1_a"), Bytes::from("2")),
+    //             (Bytes::from("table1_a"), Bytes::from("1")),
+    //             (Bytes::from("table1_b"), Bytes::new()),
+    //             (Bytes::from("table1_b"), Bytes::from("1")),
+    //             (Bytes::from("table1_c"), Bytes::from("2")),
+    //             (Bytes::from("table1_c"), Bytes::from("1")),
+    //             (Bytes::from("table2_a"), Bytes::from("2")),
+    //             (Bytes::from("table2_b"), Bytes::new()),
+    //             (Bytes::from("table2_c"), Bytes::from("2")),
+    //         ],
+    //     );
+    //
+    //     drop(snapshot0);
+    //
+    //     storage.force_full_compaction().unwrap();
+    //
+    //     let mut iter = construct_merge_iterator_over_storage(&storage.inner.state.read());
+    //     check_iter_result_by_key(
+    //         &mut iter,
+    //         vec![
+    //             (Bytes::from("table1_a"), Bytes::from("2")),
+    //             (Bytes::from("table1_c"), Bytes::from("2")),
+    //         ],
+    //     );
+    // }
 }
